5410088887f99dacd6e38bd3e01f59329ecbdef8
jxf <jinxuefen@laiyifen.com>
2018-07-09 09:50:58 +0800

    servlet完善

--- gateway/gateway_servlet/src/main/java/com/smart/servlet/web/SmartServlet.java
+++ gateway/gateway_servlet/src/main/java/com/smart/servlet/web/SmartServlet.java
@@ -44,12 +44,12 @@ public class SmartServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        this.processRequest(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        this.processRequest(req, resp);
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
